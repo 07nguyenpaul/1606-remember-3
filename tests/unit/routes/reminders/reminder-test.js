412b1190bc361a85094148b6e151a46ae22b442a
Peter Springer <peterspringer829@gmail.com>
2016-10-13 13:10:01 -0600

    show an individual reminder

--- tests/unit/routes/reminders/reminder-test.js
+++ tests/unit/routes/reminders/reminder-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:reminder', 'Unit | Route | reminder', {
+moduleFor('route:reminders/reminder', 'Unit | Route | reminders/reminder', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
