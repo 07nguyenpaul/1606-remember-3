9abe2d753ba2a939dde9fe678f1ab4d7131a509e
Pete Springer <peterspringer829@gmail.com>
2016-10-13 14:01:34 -0600

    Implement individual reminder
    * WIP issue 3
    
    * show an individual reminder

--- tests/acceptance/reminder-list-test.js
+++ tests/acceptance/reminder-list-test.js
@@ -24,7 +24,7 @@ test('viewing the homepage', function(assert) {
   });
 });
 
-skip('clicking on an individual item', function(assert) {
+test('clicking on an individual item', function(assert) {
 
   visit('/');
   click('.spec-reminder-item:first');
